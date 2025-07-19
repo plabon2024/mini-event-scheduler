e9b4f7657457f9757a950b0ced561e7e2345cad8
sheeedplabon <you@example.com>
2025-07-19 22:11:15 +0600

    Initial Setup

---
+++ mini-event-scheduler-server/index.ts
@@ -0,0 +1,14 @@
+
+
+import express, { Application, Request, Response } from 'express';
+
+const app: Application = express();
+const port: number = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send("hello");
+});
+
+app.listen(port, () => {
+  console.log(`Connected successfully on port ${port}`);
+});
